cc15dd48354c188dc6752f9c36276f4032e3b212
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-07-23 12:22:47 +0530

    Linked and beautified registraiton page. Added Crispy Forms

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'basic_blog.apps.BasicBlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
