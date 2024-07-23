a164dc3fdc997c288354d7aaa2bab39fc6f38a11
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-07-23 14:18:53 +0530

    Added Login/Logout along with profile

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 # Importing Admin to get access to admintools URL
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 # Importing path for pathing
 # Include to include the URLs inside the app URL files
 from django.urls import path, include
@@ -24,5 +24,9 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin Tools
     path('register/', users_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('profile/', users_views.profile, name="profile"),
+
     path('', include('basic_blog.urls'))  # Blog URLs
 ]
